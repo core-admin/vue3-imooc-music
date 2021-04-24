f3dea16df230ab9f25dd37efaa7d407bdd66c8a3
core-admin <xk1242425209@163.com>
2021-04-24 21:32:15 +0800

    处理拖拽面板时 歌曲列表会触发y轴方向的偏移

--- src/components/player/useMiddleInteractive.js
+++ src/components/player/useMiddleInteractive.js
@@ -11,11 +11,27 @@ export default function useMiddleInteractive() {
 
   function onMiddleTouchStart(e) {
     touch.touchStartX = e.touches[0].pageX
+
+    // 处理水平触摸拖动时 排除纵向偏移产生的影响
+    touch.touchStartY = e.touches[0].pageY
+    touch.directionLocked = ''
   }
 
   function onMiddleTouchMove(e) {
     const winWidth = window.innerWidth
     const deltaX = e.touches[0].pageX - touch.touchStartX
+    const deltaY = e.touches[0].pageY - touch.touchStartY
+
+    const absDeltaX = Math.abs(deltaX)
+    const absDeltaY = Math.abs(deltaY)
+
+    if (!touch.directionLocked) {
+      touch.directionLocked = absDeltaX >= absDeltaY ? 'h' : 'v'
+    }
+
+    // 纵向偏移 什么都不做
+    if (touch.directionLocked === 'v') return
+
     const left = currentView === 'cd' ? 0 : -winWidth
     // 限定偏移范围 0 ~ -winWidth 之间
     const offsetWidth = Math.min(0, Math.max(-winWidth, left + deltaX))
