8c7a0a52ffea16cefb24dd36217823b431966085
core-admin <xk1242425209@163.com>
2021-06-02 21:45:25 +0800

    搜索页面搜索框开发

---
+++ src/components/search/SearchInput.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="search-input">
+    <i class="icon-search"></i>
+    <input class="input-inner" v-model="query" />
+    <i v-show="query" class="icon-dismiss" @click="query = ''"></i>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { debounce } from 'throttle-debounce'
+
+export default defineComponent({
+  name: 'SearchInput',
+  props: {
+    modelValue: String,
+    placeholder: {
+      type: String,
+      default: '搜索歌曲、歌手'
+    }
+  },
+  emits: ['update:modelValue'],
+  data() {
+    return {
+      query: this.modelValue
+    }
+  },
+  created() {
+    this.$watch(
+      'query',
+      debounce(300, newQuery => {
+        this.$emit('update:modelValue', newQuery.trim())
+      })
+    )
+
+    this.$watch('modelValue', val => (this.query = val))
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.search-input {
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 0 6px;
+  height: 32px;
+  background: $color-highlight-background;
+  border-radius: 6px;
+  .icon-search {
+    font-size: 24px;
+    color: $color-text-d;
+  }
+  .input-inner {
+    flex: 1;
+    margin: 0 5px;
+    line-height: 18px;
+    background: $color-highlight-background;
+    color: $color-text;
+    font-size: $font-size-medium;
+    outline: 0;
+    &::placeholder {
+      color: $color-text-d;
+    }
+  }
+  .icon-dismiss {
+    font-size: 16px;
+    color: $color-text-d;
+  }
+}
+</style>
--- src/views/search.vue
+++ src/views/search.vue
@@ -1,16 +1,90 @@
 <template>
-  <div>search</div>
+  <div class="search">
+    <div class="search-input-wrapper">
+      <search-input v-model="query" />
+    </div>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref, watch } from 'vue'
+import SearchInput from '@/components/search/SearchInput'
 
 export default defineComponent({
-  name: 'search',
+  name: 'Search',
+  components: {
+    SearchInput
+  },
   setup() {
-    return {}
+    const query = ref(null)
+
+    watch(query, val => {
+      console.log(val)
+    })
+
+    return {
+      query
+    }
   }
 })
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.search {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  .search-input-wrapper {
+    margin: 20px;
+  }
+  .search-content {
+    flex: 1;
+    overflow: hidden;
+    .hot-keys {
+      margin: 0 20px 20px 20px;
+      .title {
+        margin-bottom: 20px;
+        font-size: $font-size-medium;
+        color: $color-text-l;
+      }
+      .item {
+        display: inline-block;
+        padding: 5px 10px;
+        margin: 0 20px 10px 0;
+        border-radius: 6px;
+        background: $color-highlight-background;
+        font-size: $font-size-medium;
+        color: $color-text-d;
+      }
+    }
+    .search-history {
+      position: relative;
+      margin: 0 20px;
+      .title {
+        display: flex;
+        align-items: center;
+        height: 40px;
+        font-size: $font-size-medium;
+        color: $color-text-l;
+        .text {
+          flex: 1;
+        }
+        .clear {
+          @include extend-click();
+          .icon-clear {
+            font-size: $font-size-medium;
+            color: $color-text-d;
+          }
+        }
+      }
+    }
+  }
+  .search-result {
+    flex: 1;
+    overflow: hidden;
+  }
+}
+</style>
